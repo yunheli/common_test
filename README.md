a19aec36f355b3f923df1193e6bedebd884da90a
Will <yh@fir.im>
2017-02-14 16:51:10 +0800

    add echo

---
+++ README.md
@@ -0,0 +1,16 @@
+# hello_word Step
+echo hello_word
+
+### INPUTS
+
+
+## EXAMPLE 
+
+```yml
+steps:
+  - name: hello_word
+    enable: true
+    failure: true
+    plugin:
+      name: hello_word
+```
---
+++ run.sh
@@ -0,0 +1,17 @@
+# ************************************************************
+#
+# This step will use email to send msg
+#
+#   Variables used:
+#     $FLOW_EMAIL_TO
+#     $FLOW_EMAIL_SUBJECT
+#     $FLOW_USER_ID
+#     $FLOW_EMAIL_CONTENT
+#
+#   Outputs:
+#
+# ************************************************************
+
+set +e
+
+echo "hello world"
